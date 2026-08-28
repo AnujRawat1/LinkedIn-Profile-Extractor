17176ae6ffcc39d08e3bb7813703a7365554965a
AnujRawat1 <rawatanuj058gmail.com>
2026-08-28 20:33:30 +0530

    Initial Project Setup

---
+++ src/main/java/com/anuj/LinkedinProfileExtractor/LinkedinProfileExtractorApplication.java
@@ -0,0 +1,13 @@
+package com.anuj.LinkedinProfileExtractor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinkedinProfileExtractorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LinkedinProfileExtractorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/anuj/LinkedinProfileExtractor/LinkedinProfileExtractorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anuj.LinkedinProfileExtractor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LinkedinProfileExtractorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
